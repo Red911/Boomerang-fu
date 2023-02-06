d341ba031bbc4394c0e89a292ffc999e699b3840
Romain <mromain911@gmail.com>
2023-02-06 22:23:37 +0100

    multi part 1

---
+++ BoomerangFu/Assets/Script/Multiplayer/PlayerConfigurationManager.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PlayerConfigurationManager : MonoBehaviour
+{
+    public string LevelName;
+    
+    private List<PlayerConfiguration> _playerConfigs;
+
+    [SerializeField]private int _maxPlayer = 4;
+    public static PlayerConfigurationManager Instance { get; set; }
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogWarning("Trying to create another instance of PlayerConfigurationManager ");
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+            _playerConfigs = new List<PlayerConfiguration>();
+        }
+    }
+
+    public void SetPlayerColor(int index, Material color)
+    {
+        _playerConfigs[index].PlayerMaterial = color;
+    }
+
+    public void ReadyPlayer(int index)
+    {
+        _playerConfigs[index].IsReady = true;
+        if (_playerConfigs.Count == _maxPlayer && _playerConfigs.All(p => p.IsReady == true))
+        {
+            SceneManager.LoadScene(LevelName);
+        }
+    }
+
+    public void HandlePlayerJoin(PlayerInput pi)
+    {
+        Debug.Log("Player Joined " + pi.playerIndex);
+
+        if (!_playerConfigs.Any(p => p.PlayerIndex == pi.playerIndex))
+        {
+            pi.transform.SetParent(transform);
+            _playerConfigs.Add(new PlayerConfiguration(pi));
+        }
+    }
+}
+
+public class PlayerConfiguration
+{
+    public PlayerConfiguration(PlayerInput pi)
+    {
+        PlayerIndex = pi.playerIndex;
+        Input = pi;
+    }
+    
+    public PlayerInput Input {get; set;}
+    public int PlayerIndex {get; set;}
+    public bool IsReady {get; set;}
+    public Material PlayerMaterial {get; set;}
+}
